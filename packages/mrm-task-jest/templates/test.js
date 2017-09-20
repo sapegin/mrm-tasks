1754b955ec7113d71d37adf9135c6e20cd6bff22
Artem Sapegin <artem@sapegin.ru>
2017-09-20 10:10:29 +0200

    Fix(jest): More useful default template

--- packages/mrm-task-jest/templates/test.js
+++ packages/mrm-task-jest/templates/test.js
@@ -1,4 +1,8 @@
-it('should work please', () => {
-	const result = 42;
-	expect(result).toBeTruthy();
+const pizza = require('./index');
+
+describe('pizza()', () => {
+	it('should work please', () => {
+		const result = pizza(42);
+		expect(result).toBeTruthy();
+	});
 });
