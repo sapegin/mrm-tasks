f6358da7935276c72b4a3475fe1654fb83ca3f3d
Artem Sapegin <artem@sapegin.ru>
2017-12-21 14:00:59 +0100

    Test: Add tests for the last commit

--- packages/mrm-task-package/index.spec.js
+++ packages/mrm-task-package/index.spec.js
@@ -10,22 +10,45 @@ const { getConfigGetter } = require('mrm');
 const vol = require('memfs').vol;
 const task = require('./index');
 
+const options = {
+	name: 'Gendalf',
+	url: 'http://middleearth.com',
+	github: 'gendalf',
+};
+
 afterEach(() => {
 	vol.reset();
 	process.chdir('/');
 });
 
 it('should add package.json', () => {
+	// The task will use the folder name as a package name
 	vol.mkdirpSync(__dirname);
 	process.chdir(__dirname);
 
-	task(
-		getConfigGetter({
-			name: 'Gendalf',
-			url: 'http://middleearth.com',
-			github: 'gendalf',
-		})
-	);
+	task(getConfigGetter(options));
 
 	expect(vol.toJSON()[path.join(__dirname, 'package.json')]).toMatchSnapshot();
+});
+
+it('should set custom Node.js version', () => {
+	task(
+		getConfigGetter(
+			Object.assign({}, options, {
+				minNode: '9.1',
+			})
+		)
+	);
+	expect(vol.toJSON()['/package.json']).toMatchSnapshot();
+});
+
+it('should set custom license', () => {
+	task(
+		getConfigGetter(
+			Object.assign({}, options, {
+				license: 'BSD',
+			})
+		)
+	);
+	expect(vol.toJSON()['/package.json']).toMatchSnapshot();
 });
